3419539581bdb19839aabd85a25bbd77a8c10904
EgorEast <jir.jir35@gmail.com>
2021-10-01 12:40:28 +0300

    reset level button is added

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { setIsStart } from '../store/gameSlice';
+import { setIsStartGame, setLvl } from '../store/gameSlice';
 
 export const StartPage = () => {
 	const dispatch = useDispatch();
@@ -8,10 +8,13 @@ export const StartPage = () => {
 	return (
 		<div className='start-page'>
 			<button
-				onClick={() => dispatch(setIsStart(true))}
+				onClick={() => dispatch(setIsStartGame(true))}
 				className='start-button'
 			>
 				START
+			</button>
+			<button onClick={() => dispatch(setLvl(0))} className='reset-button'>
+				Reset Level
 			</button>
 		</div>
 	);
