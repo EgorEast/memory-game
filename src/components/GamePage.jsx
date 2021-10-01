f159f5a776db0616ba9e5b69e1b2cea8cd9560d9
EgorEast <jir.jir35@gmail.com>
2021-10-01 13:00:45 +0300

    check on similary of two chosen tiles ins GamePage

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,15 +1,73 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { setIsStart } from '../store/gameSlice';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	setIsClosed,
+	setIsNecesMix,
+	cleanCheckedTiles,
+} from '../store/tilesSlice';
+import {
+	setIsStartGame,
+	incrementLvl,
+	setIsStartNextLvl,
+	setIsFailedLvl,
+	incrementCountGuessedCouplesTiles,
+	resetCountGuessedCouplesTiles,
+	setIsCompletedLvl,
+} from '../store/gameSlice';
+
 import { Board } from './Board';
 
 export const GamePage = () => {
 	const dispatch = useDispatch();
+	const checkedTiles = useSelector((state) => state.tiles.checkedTiles);
+	const isCompletedLvl = useSelector((state) => state.game.isCompletedLvl);
+	const numberAllCouplesTiles = useSelector(
+		(state) => state.game.numberAllCouplesTiles
+	);
+	const countGuessedCouplesTiles = useSelector(
+		(state) => state.game.countGuessedCouplesTiles
+	);
+
+	const timer = 3000;
+	useEffect(() => {
+		if (checkedTiles.length >= 2) {
+			if (checkedTiles[0].class !== checkedTiles[1].class) {
+				dispatch(
+					setIsClosed(false),
+					setIsNecesMix(true),
+					setIsFailedLvl(true),
+					resetCountGuessedCouplesTiles()
+				);
+
+				setTimeout(() => {
+					dispatch(setIsClosed(true), setIsFailedLvl(false));
+				}, timer);
+			} else {
+				dispatch(incrementCountGuessedCouplesTiles(1));
+			}
+			dispatch(cleanCheckedTiles());
+		}
+	}, [checkedTiles, dispatch]);
+
+	useEffect(() => {
+		if (numberAllCouplesTiles === countGuessedCouplesTiles) {
+			dispatch(incrementLvl(1), setIsCompletedLvl(true));
+		}
+	}, [countGuessedCouplesTiles, numberAllCouplesTiles, dispatch]);
+
+	useEffect(() => {
+		if (isCompletedLvl) {
+			dispatch(setIsStartNextLvl(true));
+		}
+	}, [dispatch, isCompletedLvl]);
+
 	return (
 		<div className='game-page'>
 			<button
 				className='back-button'
-				onClick={() => dispatch(setIsStart(false))}
+				onClick={() => {
+					dispatch(setIsStartGame(false), setIsClosed(false));
+				}}
 			></button>
 			<Board />
 		</div>
